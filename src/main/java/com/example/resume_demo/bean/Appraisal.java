fd432b350a71b6584b0ea541f49222974f667501
chellyang <83898780+chellyang@users.noreply.github.com>
2021-05-17 12:28:50 +0800

    add wenjian

---
+++ src/main/java/com/example/resume_demo/bean/Appraisal.java
@@ -0,0 +1,17 @@
+package com.example.resume_demo.bean;
+
+public class Appraisal {
+    private String content;
+
+    public Appraisal(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
