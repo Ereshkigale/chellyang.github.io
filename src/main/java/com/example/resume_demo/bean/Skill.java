f4c138800a960f818641bd6d84047109a92247a8
chellyang <83898780+chellyang@users.noreply.github.com>
2021-05-17 10:43:48 +0800

    个人简历

---
+++ src/main/java/com/example/resume_demo/bean/Skill.java
@@ -0,0 +1,17 @@
+package com.example.resume_demo.bean;
+
+public class Skill {
+    private String skillDetails;
+
+    public Skill(String skillDetails) {
+        this.skillDetails = skillDetails;
+    }
+
+    public String getSkillDetails() {
+        return skillDetails;
+    }
+
+    public void setSkillDetails(String skillDetails) {
+        this.skillDetails = skillDetails;
+    }
+}
